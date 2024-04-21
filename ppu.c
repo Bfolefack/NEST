4dc03ad18e18f8c2eaf859883ed48bb1e4aa77ec
MichaelHenryJennings <michaeljennings@utexas.edu>
2024-04-21 13:47:20 -0500

    implemented PPU registers

--- ppu.c
+++ ppu.c
@@ -1,2 +1,5 @@
 #include "system_vars.h"
+#include "ppu.h"
 #include "execute.h"
+
+PPUREGS ppu_regs;
--- ppu.h
+++ ppu.h
@@ -1,29 +1,43 @@
 #include "system_vars.h"
 typedef struct PPUCTRL {
-    uint8_t V; // NMI Enable
-    uint8_t P; // PPU Master/Slave - don't use
-    uint8_t H; // Sprite Size
-    uint8_t B; // Background Pattern Table Address
-    uint8_t S; // Sprite Pattern Table Address
-    uint8_t I;
-    uint8_t N1;
-    uint8_t N0;
+    uint8_t V : 1; // NMI Enable
+    uint8_t P : 1; // PPU Master/Slave - don't use
+    uint8_t H : 1; // Sprite Size
+    uint8_t B : 1; // Background Pattern Table Address
+    uint8_t S : 1; // Sprite Pattern Table Address
+    uint8_t I : 1;
+    uint8_t N1 : 1;
+    uint8_t N0 : 1;
 } PPUCTRL;
 
 typedef struct PPUMASK {
-    uint8_t B;
-    uint8_t G;
-    uint8_t R;
-    uint8_t s;
-    uint8_t b;
-    uint8_t M;
-    uint8_t m;
-    uint8_t G;
+    uint8_t B : 1;
+    uint8_t G : 1;
+    uint8_t R : 1;
+    uint8_t s : 1;
+    uint8_t b : 1;
+    uint8_t M : 1;
+    uint8_t m : 1;
+    uint8_t G : 1;
 } PPUMASK;
 
 
 typedef struct PPUSTATUS {
-    uint8_t V;
-    uint8_t S;
-    uint8_t O;
+    uint8_t V : 1;
+    uint8_t S : 1;
+    uint8_t O : 1;
+
 } PPUSTATUS;
+
+typedef struct PPUREGS {
+    PPUCTRL ppu_ctrl;
+    PPUMASK ppu_mask;
+    PPUSTATUS ppu_status;
+    uint8_t oam_addr;
+    uint8_t oam_data;
+    uint8_t ppu_scroll;
+    uint8_t ppu_addr;
+    uint8_t ppu_data;
+    uint8_t oam_dma;
+} PPUREGS;
+
