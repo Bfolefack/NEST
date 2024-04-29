eb793ea41097acc03fa81c91d091097aaf2eee96
MichaelHenryJennings <michaeljennings@utexas.edu>
2024-04-29 15:48:32 -0500

    / not % for secondary index

--- ppu.cpp
+++ ppu.cpp
@@ -229,13 +229,13 @@ void sprite_evaluation() {
                 uint8_t plane_1 = ppu_read(tile_index + ((tall_sprites() && y_index >= 8) << 4) | 0b1000 | (y_index % 8));
                 if (!(attributes & 0b1000000)) {
                     for (uint8_t i = 0; i < 8; i++) {
-                        uint16_t index = ((i == 7) ? (plane_1 & 1) : ((plane_1 & (1 << (7 - i))) >> (6 - i))) | ((plane_0 & (1 << (7 - i))) >> (7 - i));
+                        uint16_t index = ((i == 7) ? ((plane_1 & 1) << 1) : ((plane_1 & (1 << (7 - i))) >> (6 - i))) | ((plane_0 & (1 << (7 - i))) >> (7 - i));
                         sprite_tile_data[secondary_index][i] = (attributes & 0b100000) | ((attributes & 0b11) << 2) | index;
                         // priority flag added for muxing
                     }
                 } else { // horizontal flip
                     for (uint8_t i = 0; i < 8; i++) {
-                        uint16_t index = ((i == 0) ? (plane_1 & 1) : ((plane_1 & (1 << i)) >> (i - 1))) | ((plane_0 & (1 << i)) >> i);
+                        uint16_t index = ((i == 0) ? ((plane_1 & 1) << 1) : ((plane_1 & (1 << i)) >> (i - 1))) | ((plane_0 & (1 << i)) >> i);
                         sprite_tile_data[secondary_index][i] = (attributes & 0b100000) | ((attributes & 0b11) << 2) | index;
                         // priority flag added for muxing
                     }
